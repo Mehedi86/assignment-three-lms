5dbede5af6ee1d16488239c16314908a0d9b8b21
Mehedi86 <abdullahalmehedi@gmail.com>
2025-06-27 14:38:04 +0600

    added: filter by genre

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -3,9 +3,20 @@ import { Book } from "../models/books.model";
 
 export const bookRoutes = express.Router();
 
-bookRoutes.get('/', (req: Request, res: Response) => {
-    console.log("the right url is hitted");
-    res.send('Hello welcome, you are good to go')
+bookRoutes.get('/', async (req: Request, res: Response) => {
+    const { filter, sortBy, sort, limit } = req.query;
+
+    const readyLimit = parseInt(limit as string);
+    const readyFilter = filter ? { genre: filter } : {};
+
+
+    const books = await Book.find(readyFilter).sort({ [sortBy as string]: sort === "desc" ? -1 : 1 }).limit(readyLimit || 10)
+
+    res.json({
+        success: true,
+        message: "Books retrieved successfully",
+        data: books
+    })
 })
 
 bookRoutes.post('/', async (req: Request, res: Response) => {
