b2e9ca24974fdba0022c3a33794a421bb700dd29
Mehedi86 <abdullahalmehedi@gmail.com>
2025-06-27 11:31:49 +0600

    added: post book api

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -1,8 +1,20 @@
 import express, { Request, Response } from "express"
+import { Book } from "../models/books.model";
 
 export const bookRoutes = express.Router();
 
-bookRoutes.get('/', (req: Request, res: Response)=>{
+bookRoutes.get('/', (req: Request, res: Response) => {
     console.log("the right url is hitted");
     res.send('Hello welcome, you are good to go')
+})
+
+bookRoutes.post('/', async (req: Request, res: Response) => {
+    const data = req.body;
+    const book = await Book.create(data);
+
+    res.status(201).json({
+        success: true,
+        message: "Book created successfully",
+        data: book
+    })
 })
