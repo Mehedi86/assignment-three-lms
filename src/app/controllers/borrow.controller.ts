255501d35fb371fda08f6c0ffa5988ac69f8b2e1
Mehedi86 <abdullahalmehedi@gmail.com>
2025-06-28 05:38:42 +0600

    added: aggregate summery

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -15,3 +15,53 @@ borrowRoutes.post('/', async (req: Request, res: Response) => {
         data: borrow
     })
 })
+
+
+borrowRoutes.get('/', async (req: Request, res: Response) => {
+
+    const borrows = await Borrow.aggregate([
+        // stage 1
+        {
+            $group: {
+                _id: "$book",
+                totalQuantity: { $sum: "$quantity" }
+            }
+        },
+        // stage 2
+        {
+            $addFields: {
+                bookId: { $toObjectId: "$_id" }
+            }
+        },
+        // stage 3
+        {
+            $lookup: {
+                from: "books",
+                localField: "bookId",
+                foreignField: "_id",
+                as: "bookDetails"
+            }
+        },
+        // stage 4
+        {
+            $unwind: "$bookDetails"
+        },
+        // stage 5
+        {
+            $project: {
+                _id: 0,
+                book: {
+                    title: "$bookDetails.title",
+                    isbn: "$bookDetails.isbn"
+                },
+                totalQuantity: 1
+            }
+        }
+    ]);
+
+    res.status(201).json({
+        success: true,
+        message: "Borrowed books summary retrieved successfully",
+        data: borrows
+    })
+})
